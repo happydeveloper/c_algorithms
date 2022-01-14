b11a96827f1823731e7f604f5dadde17eb644446
duru <enfn2001@gmail.com>
2022-01-15 05:40:08 +0900

    recf: short code 알고리즘 풀기

--- add_cycle.c
+++ add_cycle.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 int solution(int n){
-  int j, n_1, n_2, sum=0, cnt=0;
+  int j, cnt=0;
   j = n;
   while(1){
-    n_1 = n/10, n_2 = n%10;
-    sum = n_1 + n_2;
-    if(sum >= 10) sum = sum%10;
-    sum = n_2 * 10 + sum;
-    if(j == sum) break;
-    n=sum;
+    j  = (j%10) * 10 + ((j/10 + j%10) % 10);
     cnt++;
-  }
-  return cnt; 
+    if(n == j) return cnt;
+  } 
 }
+
 int main() {
   int N;
   scanf("%d", &N );
